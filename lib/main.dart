d6ec3a54ac049ae6af0c9c58f07efe336ab62b36
tnagasak <n.agasakito@gmail.com>
2021-02-11 22:55:06 +0900

    modified

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-
+import 'package:path/path.dart' as path;
 
 void main() {
   runApp(MyApp());
@@ -21,43 +21,61 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
 class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("WebView")),
-      body: SamplePage(),
-      );
+      appBar: AppBar(title: Text("WebView")),
+      body: WebViewPage(),
+    );
   }
 }
 
-class SamplePage extends StatefulWidget {
+class WebViewPage extends StatefulWidget {
   @override
-  _SamplePageState createState() => _SamplePageState();
+  _WebViewPageState createState() => _WebViewPageState();
 }
 
-class _SamplePageState extends State<SamplePage> {
+class _WebViewPageState extends State<WebViewPage> {
+  //
   static const _initialUrl = 'https://www.google.com/?hl=ja';
 
-  WebAndPdfViewController _controller;
+  WebViewController _webViewController;
 
-  @override
-  void initState() {
-    super.initState();
-  }
+  void onPageStarted(String originalUriString) {
+    //
+    //
+    try {
+      final originalUri = Uri.parse(originalUriString);
+      final ext = path.extension(originalUri.path);
 
-  @override
-  void dispose() {
-    super.dispose();
+      print('ext: $ext');
+
+      if (ext == '.pdf' && Platform.isAndroid) {
+        //
+        // https:///viewer?url=
+        //
+        final redirectUrl = Uri.https(
+          'docs.google.com',
+          '/viewer',
+          {
+            'url': originalUriString
+          }
+        );
+
+        print('redirect to $redirectUrl');
+        _webViewController?.loadUrl(redirectUrl.toString());
+      }
+
+    } catch (error) {
+      //
+      print('parse error: $error');
+    }
   }
 
   @override
@@ -67,38 +85,39 @@ class _SamplePageState extends State<SamplePage> {
         Expanded(
             flex: 1,
             child: WebView(
-              initialUrl: _SamplePageState._initialUrl,
+              initialUrl: _WebViewPageState._initialUrl,
               javascriptMode: JavascriptMode.unrestricted,
-              onWebViewCreated: (WebViewController webViewController){
-                _controller = webViewController;
-              },
+              onWebViewCreated: (WebViewController webViewController) =>
+                  _webViewController = webViewController,
+              onPageStarted: onPageStarted,
             )),
         Container(
           color: Colors.white,
           height: 20,
           //省略
-          ),
-      ],);
+        ),
+      ],
+    );
   }
 }
 
-class WebAndPdfViewController extends WebViewController{
+// class WebAndPdfViewController extends WebViewController{
 
-  //下記super部分でエラー
-  WebAndPdfViewController():super();
-  final String googleDocs = "https://docs.google.com/viewer?url=";
-  @override
-  Future<void> loadUrl(String url, {Map<String, String> headers}){
-    final String newUrl = createLoadUrl(url);
-    return super.loadUrl(newUrl, headers);
-  }
+//   //下記super部分でエラー
+//   WebAndPdfViewController():super();
+//   final String googleDocs = "https://docs.google.com/viewer?url=";
+//   @override
+//   Future<void> loadUrl(String url, {Map<String, String> headers}){
+//     final String newUrl = createLoadUrl(url);
+//     return super.loadUrl(newUrl, headers);
+//   }
 
-  String createLoadUrl(String url) {
-    if (Platform.isAndroid) {
-      return googleDocs + url;
-    } else {
-      return url;
-    }
-  }
+//   String createLoadUrl(String url) {
+//     if (Platform.isAndroid) {
+//       return googleDocs + url;
+//     } else {
+//       return url;
+//     }
+//   }
 
-}
+// }
